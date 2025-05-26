dcfed3bebdae5700356f32ea7eeadcf2d498be9d
Beaugeois Carole <beaugeois@insa-20536>
2025-05-26 11:49:03 +0200

    modifs fenetre glissante

--- src/mictcp.c
+++ src/mictcp.c
@@ -90,18 +90,24 @@ int maj_fenetre_glissante(int retour_recv, int socket, mic_tcp_pdu pdu_recu, int
         }
         
     }
+
+    printf("valeur de nouvelle case : %d \n", nouvelle_case) ;
+
     tableau_fenetres[socket][index_fenetre[socket]] = nouvelle_case;
-    index_fenetre[socket]=(index_fenetre[socket]+1)%taille_fenetre_glissante;
+    // index_fenetre[socket]=(index_fenetre[socket]+1)%taille_fenetre_glissante;
 
 
     for (int i = 0 ; i < taille_fenetre_glissante ; i++) {
         moyenne += (float)tableau_fenetres[socket][i] ;
     }
     moyenne = moyenne / taille_fenetre_glissante ;
+    printf("moyenne à la fin de maj : %f\n", moyenne) ;
 
     if (moyenne < tx_pertes_admissible) {
+        printf("moyenne : %f < tx_pertes : %f, on return 0 tout va bien\n", moyenne, tx_pertes_admissible) ;
         return 0 ;
     } else {
+        printf("moyenne : %f > tx_pertes : %f, on return 1 il faut renvoyer le pdu\n", moyenne, tx_pertes_admissible) ;
         return 1 ;
     }
 
@@ -161,28 +167,36 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
 
     mic_tcp_pdu pdu_recu ; 
 
+    
+
     while((recu < 1) && k<10) { /* tant qu'on n'a pas fait trop d'itérations, 
         et tant que l'accusé de réception n'est pas reçu, 
         sachant que son numéro doit correspondre au numéro de séquence du pdu contenu dans le buffer*/
         
         retour_recv = IP_recv(&pdu_recu, &le_socket.local_addr.ip_addr, &le_socket.remote_addr.ip_addr, timer) ; // TODO: mettre les arguments FAIT
-        printf("retour_recv : %d\n", retour_recv) ;
+        printf("     RETOUR_RECV : %d\n", retour_recv) ;
         int retour_fenetre_glissante = maj_fenetre_glissante(retour_recv, mic_sock, pdu_recu, buffer[0].header.seq_num) ;
 
-        /*printf("pdu_recu.header.ack_num : %d\n", pdu_recu.header.ack_num) ;
-        printf("buffer[0].header.seq_num : %d\n", buffer[0].header.seq_num) ;*/
+        printf("pdu_recu.header.ack_num : %d\n", pdu_recu.header.ack_num) ;
+        printf("buffer[0].header.seq_num : %d\n", buffer[0].header.seq_num) ;
 
-        if (retour_fenetre_glissante == 0){ //on vérifie que le PDU reçu soit bien un ack 
+        //on vérifie que le PDU reçu soit bien un ack 
+        if (retour_fenetre_glissante == 0 || ((pdu_recu.header.ack == 1) && (pdu_recu.header.ack_num == buffer[0].header.seq_num))){ 
+            printf("retour fenetre glissante : %d, on met recu à 1 car soit f_g = 0, soit c'est le bonn ack \n", retour_fenetre_glissante) ;
             recu = 1 ;
         } else if (retour_fenetre_glissante == 1) {
             /*printf("pdu_recu.header.ack_num : %d\n", pdu_recu.header.ack_num) ;
             printf("buffer[0].header.seq_num : %d\n", buffer[0].header.seq_num) ;*/
+            printf("retour fenetre glissante : %d, on met recu à -1 \n", retour_fenetre_glissante) ;
             sent_size = IP_send(buffer[0], le_socket.remote_addr.ip_addr) ; 
             retour_recv = -1;
-            printf("sent_size : %d\n",sent_size) ;
+            printf("     SENT_SIZE : %d\n",sent_size) ;
         }
         k++ ;
     }
+
+    index_fenetre[mic_sock]=(index_fenetre[mic_sock]+1)%taille_fenetre_glissante;
+
     if(k < 10) {
         printf("k : %d\n", k);
         return sent_size;
@@ -253,10 +267,11 @@ void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_ip_addr local_addr, mic_tcp_i
         pdu_ack.payload.size=0;
 
         buffer[0]=pdu_ack;
-
+        
         printf("ACK NUM : %d \n",buffer[0].header.ack_num);
         IP_send(pdu_ack,remote_addr);
         pe=(pe+1)%2;
+        printf("ack maj : %d \n \n", pe);
 
     }
 
